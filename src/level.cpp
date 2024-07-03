86988829289f59a6af95ac15d75f26253ed6ea06
Victor Frangov <victor.frangov@gmail.com>
2024-07-03 19:36:38 -0400

    implemented loadmap() + tweaked .tmx path. still missing helper methods for loadmap

--- src/level.cpp
+++ src/level.cpp
@@ -26,16 +26,8 @@ void Level::update(int p_elapsedTime){
 }
 
 void Level::draw(Graphics &p_graphics){
-    SDL_Rect src = {0, 0, 64, 64};
-    SDL_Rect dst;
-    for(int x = 0; x < this->_size.x / 64; x++){
-        for(int y = 0; y < this->_size.y / 64; y++){
-            dst.x = x * 64 * globals::SPRITE_SCALE;
-            dst.y = y * 64 * globals::SPRITE_SCALE;
-            dst.w = 64 * globals::SPRITE_SCALE;
-            dst.h = 64 * globals::SPRITE_SCALE;
-            p_graphics.blitSurface(this->_backgroundTexture, &src, &dst);
-        }
+    for(int i = 0; i < this->_tileList.size(); i++){
+        this->_tileList.at(i).draw(p_graphics);
     }
 }
 
@@ -68,7 +60,7 @@ void Level::loadMap(std::string p_mapName, Graphics &p_graphics){
             const char* source = pTileset->FirstChildElement("image")->Attribute("source");
             char* path;
             std::stringstream ss;
-            ss << "../res/gfx/tilesets/" << source;
+            ss << source;
             pTileset->QueryIntAttribute("firstgid", &firstGid);
             SDL_Texture* tex = SDL_CreateTextureFromSurface(p_graphics.getRenderer(), p_graphics.loadImage(ss.str()));
             this->_tilesets.push_back(Tileset(tex, firstGid));
@@ -81,7 +73,80 @@ void Level::loadMap(std::string p_mapName, Graphics &p_graphics){
     XMLElement* pLayer = mapNode->FirstChildElement("layer");
     if(pLayer != NULL){
         while(pLayer){
+            //Loading data element
+            XMLElement* pData = pLayer->FirstChildElement("data");
+            if(pData != NULL){
+                while(pData){
+                    //Loading tile element
+                    XMLElement* pTile = pData->FirstChildElement("tile");
+                    if(pTile != NULL){
+                        int tileCounter = 0;
+                        while(pTile){
+                            //build each tile here
+                            //if gid is 0, there is no tile, continue
+                             if(pTile->IntAttribute("gid") == 0){
+                                tileCounter++;
+                                if(pTile->NextSiblingElement("tile")){
+                                    pTile = pTile->NextSiblingElement("tile");
+                                    continue;
+                                } else {
+                                    break; 
+                                }
+                             }
 
+                            int gid = pTile->IntAttribute("gid");
+                            Tileset tls;
+                            for(int i = 0; i < this->_tilesets.size(); i++){
+                                if(this->_tilesets[i].FirstGid <= gid){
+                                    tls = this->_tilesets.at(i);
+                                    break;
+                                }
+                            }
+                            if(tls.FirstGid == -1){
+                                tileCounter++;
+                                if(pTile->NextSiblingElement("tile")){
+                                    pTile = pTile->NextSiblingElement("tile");
+                                    continue;
+                                } else { // means were done
+                                    break;
+                                }
+                            }
+                            // get the position of the tile in the level
+                            int xx = 0;
+                            int yy = 0;
+
+                            xx = tileCounter % width;
+                            xx *= tileWidth;
+
+                            yy += tileHeight * (tileCounter / width);
+
+                            Vector2f finalTilePos = Vector2f(xx, yy);
+
+                            //calculate the position of the tile in its tileset
+                            int tilesetWidth, tilesetHeight;
+                            SDL_QueryTexture(tls.Texture, NULL, NULL, &tilesetWidth, &tilesetHeight);
+                            int tsxx = (gid - 1) % (tilesetWidth/tileWidth) * tileWidth;
+                            tsxx *= tileWidth;
+                            int tsyy = 0;
+                            int amt = (gid / (tilesetWidth / tileWidth));
+                            tsyy = tileHeight * amt;
+                            Vector2f finalTileSetPos = Vector2f(tsxx, tsyy);
+
+                            //build the tile and add it to the level's tile list
+                            Tile tile(tls.Texture, Vector2f(tileWidth, tileHeight),
+                                finalTileSetPos, finalTilePos);
+                            this->_tileList.push_back(tile);
+                            tileCounter++;
+
+                             pTile = pTile->NextSiblingElement("tile");
+                        }
+                    }
+
+
+                    pData = pData->NextSiblingElement("data");
+                }
+            }
+            
             pLayer = pLayer->NextSiblingElement("layer");
         }
     }
